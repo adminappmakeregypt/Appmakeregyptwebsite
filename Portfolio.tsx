8f781971e78989fd7ffd0c0b8850705f4d5bd1ff
adminappmakeregypt <admin@appmakeregypt.com>
2026-07-06 21:40:11 +0300

    Add files via upload

---
+++ Portfolio.tsx
@@ -0,0 +1,11 @@
+[core]
+	repositoryformatversion = 0
+	filemode = true
+	bare = false
+	logallrefupdates = true
+[remote "origin"]
+	url = https://t:eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3ODU2NjMzNTIsImlhdCI6MTc4MzA3MTM1MiwiaXNzIjoibG92YWJsZSIsInJlcG8iOiJ3b3Jrc3BhY2VfN2h1Q3pSRnhxTUxZczFWdU44b1IiLCJzY29wZXMiOlsiZ2l0OnJlYWQiXSwic3ViIjoibG92YWJsZS1hcGkifQ.xHXldxJvoHdb4C-KJfrQ5C1VlfvrMLJfQtvY0Bq-CKqFWo99CtzHpeiA9fu6VXQA8ypbN_4SEwfTYTxD5Gs9AQ@git.private.lovable-gcp.code.storage/workspace_7huCzRFxqMLYs1VuN8oR.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+	remote = origin
+	merge = refs/heads/master
